2aee596cf1a0e68c255cd06212527c7729425388
feilong <feilongphone@gmail.com>
2019-01-10 20:15:55 +0800

    修改：     build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo zip ...
 mkdir -p lib/you_get
-cp -rv you_get/src/you_get lib/you_get
+cp -rv you_get/src/you_get lib/
 rm -rv you_get
 cd ..
 zip -r script.module.you_get.zip ./*
