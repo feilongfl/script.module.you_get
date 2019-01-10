567d844fbbddee4e1e4ec7224eb72b5dce70cbf0
feilong <feilongphone@gmail.com>
2019-01-10 19:28:32 +0800

    修改：     build.sh 	删除：     lib/you_get

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mkdir build
-pushd build
-echo git clone ...
-git clone git clone --recursive https://github.com/feilongfl/script.module.you_get.git script.module.you_get
 echo zip ...
-zip -r ../script.module.you_get.zip ./*
-popd
+cp -rv you_get/src/you_get lib/you_get
+rm -rv you_get
+pushd ..
+zip -r script.module.you_get.zip ./*
+mv script.module.you_get.zip ./script.module.you_get/
+
 
